70b031f54970a9467e991051ced778103837a11b
colov <colov@ZHIDKY>
2024-12-06 17:07:03 +0500

    Прописал переменные необходимые для работы сервера Нецветаев

--- Server/User.cs
+++ Server/User.cs
@@ -1,9 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Text;
 
 namespace Server
 {
+
+    public static List<User> Users = new List<User>();
+    public static IPAddress IpAdress;
+    public static int Port;
+
     public class User
     {
         public string login { get; set; }
